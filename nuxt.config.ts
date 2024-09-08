ac06854d74f4394f5cb2a325c9e44afa5d1aad88
Julie Ng <me@julie.io>
2024-09-08 09:41:58 +0200

    add 🍍 pinia module

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -2,8 +2,11 @@
 export default defineNuxtConfig({
   compatibilityDate: '2024-04-03',
   devtools: { enabled: true },
+
   css: [
     '~/assets/bulma.scss',
     '~/assets/main.scss'
-  ]
+  ],
+
+  modules: ['@pinia/nuxt']
 })
