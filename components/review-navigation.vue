c1448d186726e7d83b78d6d25e6c8509068511c9
Julie Ng <me@julie.io>
2024-09-09 10:53:33 +0200

    sidenav: use collapse-able details elements

--- components/review-navigation.vue
+++ components/review-navigation.vue
@@ -17,31 +17,25 @@ const props = defineProps({
 
 <template>
   <nav class="mt-4">
-    <ul class="categories mr-4">
-      <li v-for="(cat, catKey) in props.items">
-        <NuxtLink :to="anchorLink(catKey)" class="has-text-weight-semibold is-size-7">{{ props.categories[catKey] }}</NuxtLink>
+    <div class="categories mr-4">
+      <details v-for="(cat, catKey) in props.items" class="my-2">
+        <summary>
+          <NuxtLink :to="anchorLink(catKey)" class="has-text-weight-semibold is-size-7">{{ props.categories[catKey] }}</NuxtLink>
+        </summary>
         <ul class="subcategories mt-1 mb-4">
           <li v-for="(subcat, subcatKey) in cat" class="ml-4 is-size-7">
             <NuxtLink :to="anchorLink(subcatKey)">{{ props.subcategories[subcatKey] }}</NuxtLink>
           </li>
         </ul>
-      </li>
-    </ul>
+      </details>
+    </div>
   </nav>
 </template>
 
 
 <style lang="scss" scoped>
   nav {
-    // border: 1px solid red;
-
-    li {
-      li {
-        list-style-type: circle;
-      }
-    }
-
-    .categories > li {
+    summary {
       line-height: 1.1em;
     }
 
@@ -53,4 +47,16 @@ const props = defineProps({
       color: var(--bulma-text-color);
     }
   }
+
+  details {
+  summary {
+    cursor: pointer;
+
+    & > * {
+      display: inline;
+      margin-left: .25em;
+    }
+  }
+}
+
 </style>
