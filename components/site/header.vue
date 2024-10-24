d54e77d12befd452070e38670d1d0d1cebf80535
Julie Ng <me@julie.io>
2024-10-24 12:00:56 +0200

    does it work if I remove remove .woff font?

--- components/site/header.vue
+++ components/site/header.vue
@@ -1,12 +1,13 @@
-<script></script>
 <template>
   <nav class="navbar" role="navigation" aria-label="main navigation">
-    <div class="container">
-      <div class="navbar-brand is-size-5 has-text-weight-semibold">
-        <a class="navbar-item" href="/">
+    <div class="container is-size-5">
+      <div class="navbar-brand">
+        <span class="navbar-item pr-1">
+          <img src="/images/azure-logo-48px.svg">
+        </span>
+        <span class="navbar-item px-1 has-text-weight-bold has-msft-blue-color">
           Azure Review Checklists
-        </a>
-
+        </span>
         <a role="button" class="navbar-burger" aria-label="menu" aria-expanded="false" data-target="navbarBasicExample">
           <span aria-hidden="true"></span>
           <span aria-hidden="true"></span>
@@ -14,7 +15,22 @@
           <span aria-hidden="true"></span>
         </a>
       </div>
+
+      <div class="navbar-end is-size-6">
+        <div class="navbar-item">
+          Checklists
+        </div>
+        <div class="navbar-item">
+          About
+        </div>
+      </div>
     </div>
   </nav>
 </template>
 
+<style lang="scss">
+.navbar {
+  background: none;
+  border-bottom: 1px solid var(--msft-warm-light-grey);
+}
+</style>
