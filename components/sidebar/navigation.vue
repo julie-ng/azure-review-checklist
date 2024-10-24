c89db85375a022f9ae220316d6a93fe5c6e217a8
Julie Ng <me@julie.io>
2024-10-24 20:58:25 +0200

    sidebar-nav: add ClientOnly

--- components/sidebar/navigation.vue
+++ components/sidebar/navigation.vue
@@ -1,13 +1,15 @@
-
-
 <template>
   <div>
-    <p class="mt-0 mb-2 is-size-7 has-text-weight-semibold is-uppercase has-msft-cool-grey-color">Checklist</p>
-    <ChecklistDropdown />
+    <ClientOnly>
+      <p class="mt-0 mb-2 is-size-7 has-text-weight-semibold is-uppercase has-msft-cool-grey-color">
+        Checklist
+      </p>
+      <ChecklistDropdown />
 
-    <p class="mt-5 mb-2 is-size-7 has-text-weight-semibold is-uppercase has-msft-cool-grey-color">Contents</p>
-    <ChecklistNavigation />
+      <p class="mt-5 mb-2 is-size-7 has-text-weight-semibold is-uppercase has-msft-cool-grey-color">
+        Contents
+      </p>
+      <ChecklistNavigation />
+    </ClientOnly>
   </div>
 </template>
-
-
