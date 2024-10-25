82402333049662984bc11106f71a313a51047d9b
Julie Ng <me@julie.io>
2024-10-25 10:49:17 +0200

    chore: client plugin, not sure needed if we go 100% static

--- plugins/1.init-checklist.client.js
+++
@@ -1,8 +0,0 @@
-import { useChecklistStore } from '~/stores/ChecklistStore'
-
-export default defineNuxtPlugin(async (app) => {
-  log.frontend(`[🔌 Plugin] initialize checklist store`)
-
-  const checklistStore = useChecklistStore(app.$pinia)
-  await checklistStore.init()
-})
