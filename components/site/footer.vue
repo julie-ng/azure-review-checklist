050ebd7547461ae0076d430fe107a5d1e8718b23
Julie Ng <me@julie.io>
2024-11-13 11:35:51 +0100

    chore(footer): clean up leftover code/text

--- components/site/footer.vue
+++ components/site/footer.vue
@@ -7,10 +7,6 @@
       <p class="has-text-grey">
         Based on <a href="https://github.com/Azure/review-checklists" target="_blank">github.com/Azure/review-checklists</a>
       </p>
-
-      <h3 class="has-text-weight-semibold mt-3 mb-1">Debugging</h3>
-      <p>
-      </p>
     </div>
   </footer>
 </template>
